37709b2e982227f015e2f432748b53f43fdb4d40
mmolliss <malin.vannesjo@gmail.com>
2021-06-08 15:52:39 +0200

    Functioning but unstyled hamburger pop-out menu

--- final-project/frontend/src/components/NavBar.jsx
+++ final-project/frontend/src/components/NavBar.jsx
@@ -1,26 +1,34 @@
 import React from 'react'
 import { slide as Menu } from 'react-burger-menu';
-// import { FaBars } from "react-icons/fa";
+import { useRouteMatch, Link } from "react-router-dom";
 
 // import '/hamburger-styling.css'
 
 import '../../src/index.css'
 
 export const NavBar = () => {
-    const showSettings = (event) => {
+    // const showSettings = (event) => {
         
-      }
-        // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
+    //   }
         return (
           <Menu right>
-            <a id="home" className="menu-item" href="/">Home</a>
-            <a id="about" className="menu-item" href="/about">About</a>
-            <a id="contact" className="menu-item" href="/contact">Contact</a>
-            <a onClick={ showSettings } className="menu-item--small" href="">Settings</a>
+            <a href="/" id="home" className="menu-item">Home</a>
+            <a href="/about" id="about" className="menu-item">About</a>
+            <a href="/projects" id="contact" className="menu-item">Projects</a>
+            <a href="/upload" id="contact" className="menu-item">Upload</a>
+            <a href="/signup" id="contact" className="menu-item">Sign Up!</a>
+            {/* <a  to="/" onClick={ showSettings } className="menu-item--small">Settings</a> */}
           </Menu>
           
         );
   }
 
 
-
+{/* <Menu right>
+  <Link to="/" id="home" className="menu-item">Home</Link>
+  <Link to="/about" id="about" className="menu-item">About</Link>
+  <Link to="/projects" id="contact" className="menu-item">Projects</Link>
+  <Link to="/upload" id="contact" className="menu-item">Upload</Link>
+  <Link to="/signup" id="contact" className="menu-item">Sign Up!</Link>
+  <Link  to="/" onClick={ showSettings } className="menu-item--small">Settings</Link>
+</Menu> */}
