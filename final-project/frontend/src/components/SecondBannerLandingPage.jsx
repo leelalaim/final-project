3b4e94d2bbcf3bc4b6dbdd412c26fd9046d942f2
mmolliss <malin.vannesjo@gmail.com>
2021-06-10 10:49:56 +0200

    Added text to second banner on landing page

--- final-project/frontend/src/components/SecondBannerLandingPage.jsx
+++ final-project/frontend/src/components/SecondBannerLandingPage.jsx
@@ -2,16 +2,40 @@ import React from 'react'
 import styled from "styled-components/macro";
 import SecondBanner from 'assets/joanna-kosinska-1_CMoFsPfso-unsplash.jpg'
 
+const Container = styled.section`
+  position: relative;
+`;
+
 const Banner = styled.img`
   object-fit: cover;
   width: 100vw;
   height: 40vh;
 `;
 
+const TextContainer = styled.div`
+  position: absolute;
+  top: 10%;
+  left: 15%;
+`;
+
+const Text = styled.h1`
+  color: white;
+  font-size: 50px;
+  margin-bottom: -50px;
+`;
+
+const LowerText = styled(Text)`
+  color: #f4e03f;
+`;
+
 export const SecondBannerLandingPage = () => {
   return (
-    <div>
+    <Container>
       <Banner src={SecondBanner} />
-    </div>
+      <TextContainer>
+        <Text>Want to share</Text>
+        <LowerText>your project?</LowerText>
+      </TextContainer>
+    </Container>
   )
 }
