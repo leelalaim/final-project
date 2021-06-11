c4adb5a2d6a8ce26cebd7aa26d8e63de7aa0f243
mmolliss <malin.vannesjo@gmail.com>
2021-06-11 22:37:47 +0200

    Added material UI to sign up

--- final-project/frontend/src/pages/SignUp.jsx
+++ final-project/frontend/src/pages/SignUp.jsx
@@ -2,51 +2,79 @@
 import React from "react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import styled from "styled-components"
+import styled from "styled-components";
+
+// Material UI
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 
 //Inner Dependencies
 import { fetchSignUp } from "../reducers/user";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+}));
+
 const SignUpForm = styled.section`
-margin-top: 100px;
-`
+  margin-top: 100px;
+`;
+const Button = styled.button`
+  background-color: #f5c81e;
+  color: white;
+  border-radius: 50px;
+  border: none;
+  width: 50%;
+  padding: 10px;
+  font-weight: bold;
+`;
 
 export const SignUp = () => {
+  const classes = useStyles();
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const errorMessage = useSelector((store) => store.user.errors)
+  const errorMessage = useSelector((store) => store.user.errors);
 
   const onFormSubmit = (e) => {
     e.preventDefault();
     dispatch(fetchSignUp(email, password));
   };
 
-
   return (
     <SignUpForm>
-      <form onSubmit={onFormSubmit}>
-        <label>Email</label>
-        <input
-          type="text"
+      <form
+        onSubmit={onFormSubmit}
+        className={classes.root}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="standard-basic"
+          label="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-        ></input>
-        <label>Password</label>
-        <input
+        />
+        <TextField
+          id="standard-basic"
+          label="Password"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-        ></input>
-        <button
+        />
+        <Button
           type="submit"
           onClick={() => {
             console.log("CLICK");
           }}
         >
           Sign Up!
-        </button>
+        </Button>
       </form>
       <p>{errorMessage && errorMessage.errorCode}</p>
     </SignUpForm>
