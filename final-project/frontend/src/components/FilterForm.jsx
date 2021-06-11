4b45c58ffab4a44d779ff0583fa0153d10838cd9
mmolliss <malin.vannesjo@gmail.com>
2021-06-11 22:07:50 +0200

    Moved filter form to a component

--- final-project/frontend/src/components/FilterForm.jsx
+++ final-project/frontend/src/components/FilterForm.jsx
@@ -1,98 +1,134 @@
-// import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-// import InputLabel from '@material-ui/core/InputLabel';
-// import FormHelperText from '@material-ui/core/FormHelperText';
-// import FormControl from '@material-ui/core/FormControl';
-// import Select from '@material-ui/core/Select';
-// import NativeSelect from '@material-ui/core/NativeSelect';
+import React, { useState } from "react";
 
-// const useStyles = makeStyles((theme) => ({
-//   formControl: {
-//     margin: theme.spacing(1),
-//     minWidth: 120,
-//   },
-//   selectEmpty: {
-//     marginTop: theme.spacing(2),
-//   },
-// }));
+// Material UI
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
-// const bootcamps = ['', 'Technigo', 'LeWagon', 'Salt', 'Other']
-// const stacks = ['', 'React', 'JavaScript', 'HTML', 'CSS', 'Python', 'TypeScript', 'NodeJS', 'Other']
-// const weeks = ['', '1 week', '2 week', '3 week', '4 week', '5 week', '6 week', '7 week', '8 week', '9 week', '10 week']
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
-// export const FilterForm = ({ onClick }) => {
-//   const [value, setValue] = useState([]);
-//   const classes = useStyles();
-//   const [state, setState] = React.useState({
-//     bootcamp: '',
-//     stack: '',
-//     week: '',
-//   });
+const bootcamps = ["", "Technigo", "LeWagon", "Salt", "Other"];
+const stacks = [
+  "",
+  "React",
+  "JavaScript",
+  "HTML",
+  "CSS",
+  "Python",
+  "TypeScript",
+  "NodeJS",
+  "Ruby",
+  "Ruby on Rails",
+  "Other",
+];
 
-//   const handleChange = (event) => {
-//     const name = event.target.name;
-//     setState({
-//       ...state,
-//       [name]: event.target.value,
-//     });
-//   };
+const weeks = [
+  "",
+  "Week 1",
+  "Week 2",
+  "Week 3",
+  "Week 4",
+  "Week 5",
+  "Week 6",
+  "Week 7",
+  "Week 8",
+  "Week 9",
+  "Week 10",
+  "Week 11",
+  "Week 12",
+  "Week 13",
+  "Week 14",
+  "Week 15",
+  "Week 16",
+  "Week 17",
+  "Week 18",
+  "Week 19",
+  "Week 20",
+];
 
-//   return (
-//     <form>
-//       <FormControl className={classes.formControl}> 
-//         <InputLabel htmlFor="bootcamp">Bootcamp</InputLabel>
-//         <Select
-//           native
-//           value={state.bootcamp}
-//           onChange={handleChange}
-//           inputProps={{
-//             name: 'bootcamp',
-//             id: 'bootcamp',
-//           }}
-//         >
-//           {bootcamps.map(bootcamp => (
-//             <option value={bootcamp}>{bootcamp}</option>
-//           )
-//           )}
-//         </Select>
-//       </FormControl>
-//       <FormControl className={classes.formControl}>
-//         <InputLabel htmlFor="stack">Stack</InputLabel>
-//         <Select
-//           native
-//           value={state.stack}
-//           onChange={handleChange}
-//           inputProps={{
-//             name: 'stack',
-//             id: 'stack',
-//           }}
-//         >
-//           {stacks.map(stack => (
-//             <option value={stack}> {stack}</option>
-//           ))}
+export const FilterForm = ({ onSubmit }) => {
+  const classes = useStyles();
+  const [filters, setFilters] = useState({
+    bootcamp: null,
+    stack: null,
+    week: null,
+  });
 
-//         </Select>
-//       </FormControl>
-//       <FormControl className={classes.formControl}>
-//         <InputLabel htmlFor="week">Bootcamp week</InputLabel>
-//         <Select
-//           native
-//           value={state.week}
-//           onChange={handleChange}
-//           inputProps={{
-//             name: 'week',
-//             id: 'week',
-//           }}
-//         >
-//           {weeks.map(week => (
-//             <option value={week}>{week}</option>
-//           ))}
-//         </Select>
- 
-//       </FormControl>
-//       <button onClick={fil} type="submit">
-//         Filter!
-//       </button>
-//     </form>
-//   );
-// }
+  const handleChange = (event) => {
+    const name = event.target.name;
+    setFilters({
+      ...filters,
+      [name]: event.target.value,
+    });
+  };
+
+  const onFilterForm = (e) => {
+    e.preventDefault();
+    onSubmit(filters);
+  };
+
+  return (
+    <form onSumbit={onFilterForm}>
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="bootcamp">Bootcamp</InputLabel>
+        <Select
+          native
+          value={filters.bootcamp}
+          onChange={handleChange}
+          inputProps={{
+            name: "bootcamp",
+            id: "bootcamp",
+          }}
+        >
+          {bootcamps.map((bootcamp) => (
+            <option value={bootcamp}>{bootcamp}</option>
+          ))}
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="stack">Stack</InputLabel>
+        <Select
+          native
+          value={filters.stack}
+          onChange={handleChange}
+          inputProps={{
+            name: "stack",
+            id: "stack",
+          }}
+        >
+          {stacks.map((stack) => (
+            <option value={stack}> {stack}</option>
+          ))}
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="week">Week</InputLabel>
+        <Select
+          native
+          value={filters.week}
+          onChange={handleChange}
+          inputProps={{
+            name: "week",
+            id: "week",
+          }}
+        >
+          {weeks.map((week) => (
+            <option value={week}>{week}</option>
+          ))}
+        </Select>
+      </FormControl>
+      <button onClick={onFilterForm} type="submit">
+        Filter!
+      </button>
+    </form>
+  );
+};
--- final-project/frontend/src/pages/Projects.jsx
+++ final-project/frontend/src/pages/Projects.jsx
@@ -1,57 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components/macro";
 
-// Material UI
-import { makeStyles } from "@material-ui/core/styles";
-import InputLabel from "@material-ui/core/InputLabel";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
-import NativeSelect from "@material-ui/core/NativeSelect";
-
 import { fetchProjects } from "../reducers/allProjects";
 import { ProjectsBanner } from "../components/ProjectsBanner";
-// import { Footer } from "./Footer";
 import { ProjectCard } from "../components/ProjectCard";
-// import { FilterForm } from "../components/FilterForm";
-
-// Matierial UI
-const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
-}));
-
-const bootcamps = ["", "Technigo", "LeWagon", "Salt", "Other"];
-const stacks = [
-  "",
-  "React",
-  "JavaScript",
-  "HTML",
-  "CSS",
-  "Python",
-  "TypeScript",
-  "NodeJS",
-  "Other",
-];
-const weeks = [
-  "",
-  "1 week",
-  "2 week",
-  "3 week",
-  "4 week",
-  "5 week",
-  "6 week",
-  "7 week",
-  "8 week",
-  "9 week",
-  "10 week",
-];
+import { FilterForm } from "../components/FilterForm";
 
 const ProjectCards = styled.div`
   display: flex;
@@ -59,13 +13,6 @@ const ProjectCards = styled.div`
 `;
 
 export const Projects = () => {
-  const classes = useStyles();
-  const [filters, setFilters] = useState({
-    bootcamp: null,
-    stack: null,
-    week: null,
-  });
-
   const dispatch = useDispatch();
   const projects = useSelector((store) => store.allProjects.projectList);
 
@@ -73,75 +20,16 @@ export const Projects = () => {
     dispatch(fetchProjects());
   }, [dispatch]);
 
-  const handleChange = (event) => {
-    const name = event.target.name;
-    setFilters({
-      ...filters,
-      [name]: event.target.value,
-    });
-  };
-
-  const filter = (e) => {
-    e.preventDefault();
+  const onFilterFormSubmit = (filters) => {
     dispatch(fetchProjects(filters));
   };
 
   return (
     <>
       <ProjectsBanner />
-      <form onSumbit={filter}>
-        <FormControl className={classes.formControl}>
-          <InputLabel htmlFor="bootcamp">Bootcamp</InputLabel>
-          <Select
-            native
-            value={filters.bootcamp}
-            onChange={handleChange}
-            inputProps={{
-              name: "bootcamp",
-              id: "bootcamp",
-            }}
-          >
-            {bootcamps.map((bootcamp) => (
-              <option value={bootcamp}>{bootcamp}</option>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl className={classes.formControl}>
-          <InputLabel htmlFor="stack">Stack</InputLabel>
-          <Select
-            native
-            value={filters.stack}
-            onChange={handleChange}
-            inputProps={{
-              name: "stack",
-              id: "stack",
-            }}
-          >
-            {stacks.map((stack) => (
-              <option value={stack}> {stack}</option>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl className={classes.formControl}>
-          <InputLabel htmlFor="week">Week</InputLabel>
-          <Select
-            native
-            value={filters.week}
-            onChange={handleChange}
-            inputProps={{
-              name: "week",
-              id: "week",
-            }}
-          >
-            {weeks.map((week) => (
-              <option value={week}>{week}</option>
-            ))}
-          </Select>
-        </FormControl>
-        <button onClick={filter} type="submit">
-          Filter!
-        </button>
-      </form>
+      <FilterForm
+        onSubmit={(filters) => onFilterFormSubmit(filters)}
+      ></FilterForm>
       <ProjectCards>
         {projects.map((project) => (
           <ProjectCard project={project} />
