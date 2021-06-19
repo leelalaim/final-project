5c56c4361b7409aa12b4dc42e34056c608ade05c
mmolliss <malin.vannesjo@gmail.com>
2021-06-19 08:09:38 +0200

    fixed stuff

--- backend/auth/authenticateToken.middleware.js
+++ backend/auth/authenticateToken.middleware.js
@@ -1,10 +1,20 @@
 import { jwtService } from './jwtToken.service';
 
-export function authenticateToken (req, res, next) {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
+function extractToken(req) {
+  const authHeader = req.headers['authorization'];
+  if (authHeader.startsWith('Bearer ')) {
+    return authHeader.substring(7, authHeader.length);
+  }
 
-  if (token == null) return res.sendStatus(401);
+  return null;
+}
+
+export function authenticateToken (req, res, next) {
+  const token = extractToken(req);
+
+  if (token == null || token === 'null') {
+    return res.sendStatus(401);
+  }
 
   try {
     req.user = jwtService.getAuthTokenData(token);
--- backend/project/isProjectOwner.middleware.js
+++ backend/project/isProjectOwner.middleware.js
@@ -6,13 +6,11 @@ export async function isProjectOwner (req, res, next) {
 
   const project = await Project.findById(id);
   if (!project) {
-    res.status(404).json({ message: 'Project not found' });
-    return;
+    return res.status(404).json({ message: 'Project not found' });
   }
 
   if (project.ownerId !== userId) {
-    res.status(403).json({ message: 'Only owners can perform that operation' });
-    return;
+    return res.status(403).json({ message: 'Only owners can perform that operation' });
   }
 
   next();
