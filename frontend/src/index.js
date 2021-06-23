5ec964e1f6bf8076094cfabf9967f8ac6ce004cd
mmolliss <malin.vannesjo@gmail.com>
2021-06-23 16:43:17 +0200

    Added favicoc yellow square

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,5 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { App } from './App'
+import Favicon from 'react-favicon'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <>
+    <Favicon url='https://favicon.io/emoji-favicons/yellow-square/' />
+    <App />
+  </>
+, document.getElementById('root'))
